c482aa79d5d23f533837a432b5d8df69a6490687
Salman <connect.celman@gmail.com>
2024-05-26 12:11:22 +0530

    first commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateTransformatiosPage = () => {
+  return (
+    <div>UpdateTransformatiosPage</div>
+  )
+}
+
+export default UpdateTransformatiosPage
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddTransformatioTypePage = () => {
+  return (
+    <div>AddTransformatioTypePage</div>
+  )
+}
+
+export default AddTransformatioTypePage
