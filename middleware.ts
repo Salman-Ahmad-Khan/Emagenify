2a4ca84ace8977a00d08876a9fa6fff00d7e2084
Salman <connect.celman@gmail.com>
2024-06-21 15:53:32 +0530

    feat: add stripe and payment

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/', '/api/webhooks/clerk', '/api/webhooks/stripe']);
+const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)', '/api/webhooks/clerk', '/api/webhooks/stripe']);
 
 export default clerkMiddleware((auth, request) => {
   if(!isPublicRoute(request)) {
